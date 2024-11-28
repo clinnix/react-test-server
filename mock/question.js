30a6d4976f6882ed227ae055175369772e6bac09
clinnix <agile9521@gmail.com>
2024-11-29 00:27:07 +0800

    use axio access server

--- mock/question.js
+++ mock/question.js
@@ -1,6 +1,7 @@
 const Mock = require("mockjs");
 module.exports = [
   {
+    //获取单个问卷信息
     url: "/api/question/:id",
     method: "get",
     data: Mock.mock({
@@ -9,14 +10,15 @@ module.exports = [
         id: Mock.Random.id(),
         title: Mock.Random.ctitle(),
       },
-      msg: '错误测试'
+    //   msg: '错误测试'
     }),
   },
   {
+    //创建问卷
     url: "/api/question",
     method: "post",
     data: {
-      code: 200,
+      code: 0,
       data: {
         id: Mock.Random.id(),
       },
